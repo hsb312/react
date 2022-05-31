e6f925f483eb6aa9a4e226629a271ee73ed1d0b4
hsb312 <hsb973@naver.com>
2022-05-31 11:21:58 +0900

    bootLogin

---
+++ src/components/Login/Login.js
@@ -0,0 +1,27 @@
+import "./Login.css";
+
+const Login = () => {
+    return (
+        <div className="LoginPage">
+            <div className="Box">
+                <div className="imgBox">
+                    <img
+                        src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+                        alt="Logo"></img>
+                </div>
+                <div className="LoginBox">
+                    <input type="text" placeholder="Id"></input>
+                    <input type="password" placeholder="Password"></input>
+                    <button className="LoginButton">로그인</button>
+                </div>
+            </div>
+            <div className="Box">
+                <p>
+                    계정이 없으신가요? <a href="g">가입하기</a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
